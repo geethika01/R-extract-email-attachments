d171758c94c298bd1f32f34a7e94172fc5785158
Geethika Wijewardena <Geethika.Wijewardena@racgp.org.au>
2019-10-09 09:03:03 +1100

    Added Recieve Date to filter emails by date. Added to join multiple dataframes at once

--- Extract email attachments.R
+++ Extract email attachments.R
@@ -1,25 +1,29 @@
+rm(list=ls())
 library(RDCOMClient)
 library(openxlsx)
 library(dplyr)
 library(stringr)
+library(tidyverse)
 
 setwd("C:/Users/geethika.wijewardena/Workspace/R-extract-email-attachments")
 outlook_app <- COMCreate("Outlook.Application")
 search <- outlook_app$AdvancedSearch(
   "Inbox",
-  "urn:schemas:httpmail:subject = 'GEOM0001 - Marks'"
+  "urn:schemas:httpmail:subject = 'GEO0001 - Marks'"
 )
 results <- search$Results()
 
 for (i in 1:results$Count()){
-  email <- results$Item(i)
-  attachment_file <- paste0("C:/Users/geethika.wijewardena/Workspace/R-extract-email-attachments/",email$Attachments(1)[['DisplayName']])
-  email$Attachments(1)$SaveAsFile(attachment_file)
-  df_name <- str_sub(email$Attachments(1)[['DisplayName']],1,-6)
-  data <- read.xlsx(attachment_file) %>% rename(!!df_name := "Score")
-  assign(df_name, data)
-  
+  receive_date <- as.Date("1899-12-30") + floor(results$Item(i)$ReceivedTime())
+  if(receive_date == as.Date("2019-10-09")) {
+    email <- results$Item(i)
+    attachment_file <- paste0("C:/Users/geethika.wijewardena/Workspace/R-extract-email-attachments/",email$Attachments(1)[['DisplayName']])
+    email$Attachments(1)$SaveAsFile(attachment_file)
+    df_name <- str_sub(email$Attachments(1)[['DisplayName']],1,-6)
+    data <- read.xlsx(attachment_file) %>% rename(!!df_name := "Score")
+    assign(df_name, data)
+  }
 }
 
 # Join scores
-dat <- marker_1 %>% inner_join(marker_2, by="Question")
+dat <- lapply(ls(pattern="marker"), function(x) get(x)) %>% reduce(full_join, by = "Question") 
